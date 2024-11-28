b41cd9a2e163e8047c092c610b7be209f400da8b
Alicia Blomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2024-11-28 23:10:50 +0100

    First commit

---
+++ PanoramaApp/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PanoramaApp.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Lägg till tjänster till DI-kontainern
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Konfigurera HTTP-pipelinen
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapRazorPages();
+app.Run();
